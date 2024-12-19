757844db617731489a1c6ae94efa09e567e1b4c8
simsononroad <d-github@kopaszhegy.hu>
2024-12-19 20:12:59 +0100

    icon

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import pyttsx3
 from os import system
 from daniwled import *
 
-PREFIXUM = "hey computer"
+PREFIXUM = "hey"
 PREFIXUM = PREFIXUM.lower()
+LEVEL_2_PREFIXUM = "hey computer"
+LEVEL_2_PREFIXUM = LEVEL_2_PREFIXUM.lower
+
+kiadott_parancsok = []
 
 def main():
     recognizer = speech_recognition.Recognizer()
     while True:
-        txt_szoveg = open("beszed.txt", "w")
+        
         try:
             with speech_recognition.Microphone() as mic:
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
@@ -23,20 +27,26 @@ def main():
                 print("======================")
                 print(f"Felismerve: {text}")
                 print("======================")
-                txt_szoveg.write(f"Felismerve: {text}\n")
-                if text == f"{PREFIXUM} turn off the light":
-                    wled_off()
-                elif text == f"{PREFIXUM} turn on the light":
-                    wled_on(255)
-                elif text == f"{PREFIXUM} turn on the effect":
-                    wled_aurora()
-                elif text == f"{PREFIXUM} turn off the effect":
-                    wled_on(255)
-                elif text == f"{PREFIXUM} stop":
-                    print("A program sikeresen leállt")
-                    break
+                kiadott_parancsok.append(text)
+                if PREFIXUM not in text:
+                    print(f"Nem használtad a '{PREFIXUM}' prefixumot")
                 else:
-                    print("=====================\n=====================\n    Nem felismerhető a parancs    \n=====================\n=====================")
+
+                    if text == f"{PREFIXUM} turn off the light":
+                        wled_off()
+                    elif text == f"{PREFIXUM} turn on the light":
+                        wled_on(255)
+                    elif text == f"{PREFIXUM} turn on the effect":
+                        wled_aurora()
+                    elif text == f"{PREFIXUM} turn off the effect":
+                        wled_on(255)
+                    elif text == f"{PREFIXUM} stop":
+                        print("A program sikeresen leállt")
+                        txt_szoveg = open("beszed.txt", "w")
+                        txt_szoveg.write(f"Kiadott parancsok: {kiadott_parancsok}\n")
+                        break
+                    else:
+                        print("=====================\n=====================\n    Nem felismerhető a parancs    \n=====================\n=====================")
                 
                 
         except speech_recognition.UnknownValueError:
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
+<p align="center">
+  <img src="images/wled_logo_akemi.png">
+</p>
+
 # Magyar
+- Mi az a wled: [wled weboldala](https://kno.wled.ge/)
 1. Hozz létre egy `wledip.txt`-file-t és csak a wled IP-címét írd be!
 
 # English
+- What is the wled: [wled website](https://kno.wled.ge/)
 1. Create a file with this name: `wledip.txt`. The file contain only the wled IP!
